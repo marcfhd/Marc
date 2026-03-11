2300e7f51d740f3a3740dcf0c051ddd40459268e
marcfhd <marc.fahed13@gmail.com>
2026-03-11 13:30:35 +0200

    login, signup,favorites, card and home pages

---
+++ navbar.js
@@ -0,0 +1,12 @@
+fetch("navbar.html")
+.then(response => response.text())
+.then(data => {
+  document.getElementById("navbar").innerHTML = data;
+  let page = window.location.pathname.split("/").pop();
+  let links = document.querySelectorAll(".nav-link");
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].getAttribute("href") == page) {
+      links[i].classList.add("active");
+    }
+  }
+});
