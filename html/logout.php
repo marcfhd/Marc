31cfc1d0cc9286122ea2c2eba5c6f48250673a6e
marcfhd <marc.fahed13@gmail.com>
2026-05-14 11:57:01 +0300

    ...

--- html/logout.php
+++ html/logout.php
@@ -1,16 +1,14 @@
 <?php
 session_start();
-
-// destroy all session data
 session_unset();
 session_destroy();
 
-// delete remember cookie if exists
+
 if (isset($_COOKIE["remember_user"])) {
     setcookie("remember_user", "", time() - 3600, "/");
 }
 
-// redirect to home with message
+
 header("Location: index.html?msg=logged_out");
 exit();
 ?>
